fa80825af732c5928b7cd68e153247b61a182c37
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-12-09 12:53:44 +0100

    indexers: make ByLogicalClusterPath distinct from ByLogicalClusterPathAndName

--- pkg/indexers/indexers.go
+++ pkg/indexers/indexers.go
@@ -41,6 +41,8 @@ const (
 	ByClusterResourceStateLabelKey = "ByClusterResourceStateLabelKey"
 	// ByLogicalClusterPath indexes by logical cluster path, if the annotation exists.
 	ByLogicalClusterPath = "ByLogicalClusterPath"
+	// ByLogicalClusterPathAndName indexes by logical cluster path and object name, if the annotation exists.
+	ByLogicalClusterPathAndName = "ByLogicalClusterPathAndName"
 )
 
 // IndexBySyncerFinalizerKey indexes by syncer finalizer label keys.
@@ -78,6 +80,19 @@ func IndexByClusterResourceStateLabelKey(obj interface{}) ([]string, error) {
 
 // IndexByLogicalClusterPath indexes by logical cluster path, if the annotation exists.
 func IndexByLogicalClusterPath(obj interface{}) ([]string, error) {
+	metaObj, ok := obj.(metav1.Object)
+	if !ok {
+		return []string{}, fmt.Errorf("obj is supposed to be a metav1.Object, but is %T", obj)
+	}
+	if path, found := metaObj.GetAnnotations()[tenancy.LogicalClusterPathAnnotationKey]; found {
+		return []string{logicalcluster.NewPath(path).String(), logicalcluster.From(metaObj).String()}, nil
+	}
+
+	return []string{logicalcluster.From(metaObj).String()}, nil
+}
+
+// IndexByLogicalClusterPathAndName indexes by logical cluster path and object name, if the annotation exists.
+func IndexByLogicalClusterPathAndName(obj interface{}) ([]string, error) {
 	metaObj, ok := obj.(metav1.Object)
 	if !ok {
 		return []string{}, fmt.Errorf("obj is supposed to be a metav1.Object, but is %T", obj)
